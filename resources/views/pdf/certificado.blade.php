13437268bd8e39a3b3fe0b9d93cdb78a29ac4539
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2025-07-31 22:09:26 -0500

    actu

--- resources/views/pdf/certificado.blade.php
+++ resources/views/pdf/certificado.blade.php
@@ -148,7 +148,7 @@
                 <table>
                     <tr>
                         <!-- Logo a la izquierda -->
-                        <td style="text-align: left; margin-top: 20px;">
+                        <td style="text-align: left; margin-top: 40px;">
 
                             <img src="{{ public_path('assets/img/logo.webp') }}" height="60">
                         </td>
@@ -160,7 +160,7 @@
                             <p style="margin-top:-10px;">
                                 __________________<br>
                                 <span style="font-weight: bold;">{{ $delegado->nombre }}</span><br>
-                                {{ $delegado->cargo || 'Jurado' }}<br>
+                                {{ $delegado->cargo }}<br>
                                 Alcaldía de Pereira
                             </p>
                         </td>
