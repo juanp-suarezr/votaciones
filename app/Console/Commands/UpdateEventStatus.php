3cb17459b100df4ec69dad8df52642802645bdfb
Juan Pablo Suarez Ramirez <jpsuarez40@gmail.com>
2025-09-02 00:03:49 -0500

    actu

--- app/Console/Commands/UpdateEventStatus.php
+++ app/Console/Commands/UpdateEventStatus.php
@@ -20,12 +20,12 @@ class UpdateEventStatus extends Command
     public function handle()
     {
         $now = Carbon::now();
-        Log::info("Running event status update at {$now}");
 
         // Busca los eventos con fecha de inicio pasada y estado pendiente
         $eventsToUpdate = Eventos::where('fecha_inicio', '<=', $now)
             ->where('estado', 'Pendiente')
             ->get();
+
 
         $eventsToClose = Eventos::where('fecha_fin', '<=', $now)
             ->where('estado', 'Activo')
@@ -34,8 +34,6 @@ class UpdateEventStatus extends Command
         $this->info($eventsToUpdate);
         $this->info($eventsToClose);
 
-        Log::info("Events to update: " . $eventsToUpdate);
-        Log::info("Events to close: " . $eventsToClose);
 
         // Actualiza el estado de los eventos encontrados
         foreach ($eventsToUpdate as $event) {
