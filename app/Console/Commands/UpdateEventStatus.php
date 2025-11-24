48df0768623b073f6d0124e30b35b821e6e1c76c
juanp-suarezr <juanp-suarezr@users.noreply.github.com>
2025-11-24 07:44:37 -0500

    actu

--- app/Console/Commands/UpdateEventStatus.php
+++ app/Console/Commands/UpdateEventStatus.php
@@ -38,16 +38,16 @@ class UpdateEventStatus extends Command
 
 
         // Busca los eventos con fecha de inicio pasada y estado pendiente
-        $eventsToUpdate = Eventos::with([
-            'eventos_hijos' => function ($q) {
-                $q->with([
-                    'eventos.hash_proyectos.proyecto'
-                ]);
-            }
-        ])
-            ->where('fecha_inicio', '<=', $now)
+        $eventsToUpdate = Eventos::where('fecha_inicio', '<=', $now)
             ->where('estado', 'Pendiente')
+            ->with([
+                'eventos_hijos.eventos' => function ($q) {
+                    $q->where('estado', 'Activo')
+                        ->with('hash_proyectos.proyecto');
+                }
+            ])
             ->get();
+
 
 
 
